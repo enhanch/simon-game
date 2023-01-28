27ef3f32ca0af0644db6bcfba223c58c649ceced
enhanch <45930955+enhanch@users.noreply.github.com>
2023-01-28 16:29:33 -0700

    Add functions for playing sound and animating button presses. Also add jQuery event method for grabbing button and key presses.

--- game.js
+++ game.js
@@ -1,5 +1,24 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+$(document).keypress(function() {
+    if (gamePattern.length == 0) {
+        nextSequence();
+    }
+});
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
@@ -7,7 +26,12 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
 
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
 }
