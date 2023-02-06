d9adc6c7fbbfe75baf7d6708512b7df7297b34d6
enhanch <45930955+enhanch@users.noreply.github.com>
2023-02-06 15:21:34 -0700

    Add functionality to track current level in game

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
+// Declaring global variables and arrays
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
 
+// Function to play sound
 function playSound(name) {
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
+// Function to track what button the user clicked, plays a sound and animation, and logs which button was pressed
 $(".btn").click(function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
@@ -14,6 +18,7 @@ $(".btn").click(function() {
     animatePress(userChosenColour);
 });
 
+// Function to start the game when a key is pressed
 $(document).keypress(function() {
     if (gamePattern.length == 0) {
         nextSequence();
@@ -24,9 +29,10 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
+    level++;
+    $("h1").text("Level " + level);
 }
 
 function animatePress(currentColour) {
