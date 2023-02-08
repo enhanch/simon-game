a0786a13a1cf22de2356e179e7a0588326058218
enhanch <45930955+enhanch@users.noreply.github.com>
2023-02-08 12:22:00 -0700

    Create functionality to check user answers against game answers and inform them if they are wrong or not.

--- game.js
+++ game.js
@@ -2,7 +2,37 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
 var level = 0;
+
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+        console.log("success");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        setTimeout(function() {
+            $("body").addClass("game-over");
+            $("body").removeClass("game-over");
+        }, 200);
+        console.log("wrong");
+    }
+}
+
+// Function to start the game when a key is pressed
+$(document).keypress(function() {
+    if (started === false) {
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    } else {
+        nextSequence();
+    }
+  });
 
 // Function to play sound
 function playSound(name) {
@@ -16,23 +46,18 @@ $(".btn").click(function() {
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
-});
-
-// Function to start the game when a key is pressed
-$(document).keypress(function() {
-    if (gamePattern.length == 0) {
-        nextSequence();
-    }
+    checkAnswer(userClickedPattern.length - 1);
 });
 
 function nextSequence() {
+    level++;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
-    level++;
-    $("h1").text("Level " + level);
+    userClickedPattern = [];
 }
 
 function animatePress(currentColour) {
